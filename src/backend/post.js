5cc32d42a3259b6416cc3d7401774a3d8d296f83
Alefiyah Yusufbhai Madarwala <107259961+alefiyahmadar@users.noreply.github.com>
2025-08-03 20:52:46 +0530

    initial-commit

--- src/backend/post.js
+++ src/backend/post.js
@@ -2,7 +2,7 @@ export const Posts = [
   {
     id: 0,
     username: "adarshbalika",
-    text: "hey there..",
+    text: "Travel is therapy",
     createdAt: new Date().toISOString(),
     likes: {
       likeCount: 0,
@@ -13,7 +13,18 @@ export const Posts = [
   {
     id: 1,
     username: "maryjane",
-    text: "cosmos vias",
+    text: "Capybaras are the cutest!",
+    createdAt: new Date().toISOString(),
+    likes: {
+      likeCount: 0,
+      likedBy: [],
+      dislikedBy: [],
+    }
+  },
+  {
+    id: 2,
+    username: "shubhamsoni",
+    text: "I recently visited Lucid Restro and had a great experience! The ambiance was cozy and welcoming, perfect for a relaxed meal.",
     createdAt: new Date().toISOString(),
     likes: {
       likeCount: 0,
